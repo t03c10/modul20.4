c68ba6219dc942218ba2f3105e3b846d7b330c8d
Pawel Z <t03c10@gmail.com>
2018-06-08 21:27:18 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+import routes from './routes';
+
+render(
+    <Provider store={store}>
+        <Router history={hashHistory} routes={routes}/>
+    </Provider>,
+    document.getElementById('root')
+); 
